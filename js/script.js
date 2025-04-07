45a723626ab662f54116a89faa0caa01a57fbb57
default <olivia.ten-den@stu.ocsb.ca>
2025-04-07 17:03:01 +0000

    template

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Olivia Ten Den
+// Created on: April 7, 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates the area of a trapezoid.
+ */
+function calculateVolumeOfSphere() {
+  // input
+  const radiusOfSphere = parseFloat(
+    document.getElementById("radius-of-sphere").value
+  )
+
+  // process
+  const volumeOfSphere = (4 / 3) * Math.PI * radiusOfSphere ** 3
+
+  // output
+  document.getElementById("volume").innerHTML =
+    "The volume is: " + volumeOfSphere + " cm³"
+}
